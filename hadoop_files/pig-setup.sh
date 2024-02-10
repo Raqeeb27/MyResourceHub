2432cb3c886e202921cd93d5de6054bee92d8fc6
Mohammed Abdul Raqeeb <raqeeb2709@gmail.com>
2024-02-10 14:34:06 +0530

    Added pig-setup.sh and pig_installation_guide.sh scripts

---
+++ hadoop_files/pig-setup.sh
@@ -0,0 +1,198 @@
+#!/bin/bash
+
+# Script: pig-setup.sh
+# Description: Automated setup script for PIG setup
+# Author: Mohammed Abdul Raqeeb
+# Date: 10/02/2024
+
+# ------- Hadoop PIG Setup Script -------
+
+## ===========================================================================
+### Functions
+
+# Function to check hadoop installation
+check_hadoop_availability(){
+    if ! command -v hadoop &> /dev/null; then
+        echo -e "Error: 'hadoop' command not found. Please make sure Hadoop is installed and in your PATH.\n"
+        sleep 1
+
+        echo -e "Run the below command to install and configure Hadoop if isn't installed:\nsudo apt install wget -y && cd && wget https://raw.githubusercontent.com/Raqeeb27/MyResourceHub/main/hadoop_files/hadoop-setup.sh && bash hadoop-setup.sh && exit\n"
+        sleep 1
+        
+        echo -e "Exiting....\n"
+        sleep 1
+        exit 1
+    fi
+}
+
+remove_bsdgames_if_installed(){
+    # Check if bsdgames package is installed
+    if dpkg -l | grep -q "^ii.*bsdgames"; then
+        # Remove bsdgames package
+        sudo apt-get remove --purge -y bsdgames
+        echo -e "\n'bsdgames' package has been removed.\n"
+    fi
+
+}
+
+#Function to print blank lines and sleep
+log_and_pause(){
+    sleep 2
+    echo -e "\n"
+}
+
+## --------------------------------------------------------------------------
+# Function to start SSH service
+start_ssh_service() {
+    echo "Starting SSH service..."
+
+    sudo service ssh start || { echo -e "\nError: Failed to start SSH service. \nExiting...\n"; exit 1; }
+
+    echo -e "\nSSH service started successfully."
+    log_and_pause
+}
+
+## --------------------------------------------------------------------------
+# Function to restart Hadoop services
+restart_hadoop_services() {
+    # Stop Hadoop services
+    echo -e "Stopping Hadoop services if any...\n"
+    sleep 1
+
+    stop-all.sh || { echo -e "\nError stopping Hadoop services!! \nExiting....\n"; sleep 1.5; exit 1; }
+
+    echo -e "\n\n"
+    sleep 1
+
+    # Start Hadoop services
+    echo -e "Starting Hadoop services...\n"
+    sleep 1
+
+    start-all.sh || { echo -e "\nError starting Hadoop services!! \nExiting....\n"; sleep 1.5; exit 1; }
+
+    echo -e "\nHadoop services started successfully."
+
+    sleep 1
+    log_and_pause
+}
+
+## --------------------------------------------------------------------------
+# Function to prompt user for download confirmation
+confirm_download() {
+    read -p "The PIG tar file is around 220MB in size. Do you want to proceed with the download? (Y/N): " -r choice
+
+    # Convert the user input to lowercase for case-insensitive comparison
+    case "${choice,,}" in
+        y|yes|"") return 0 ;;  # Accept 'y', 'yes', 'Y', 'YES', 'Yes', or Enter key
+        *) return 1 ;;          # Any other input is considered negative
+    esac
+}
+
+# Function to download and extract Hadoop
+download_and_extract_pig() {
+    echo -e "\nDownloading and extracting Pig..."
+    log_and_pause
+
+    # Call confirm_download to check if the user wants to proceed
+    confirm_download || { sleep 1; echo -e "\nAborting Pig installation..."; sleep 1; echo -e "Exiting...\n"; sleep 1; exit 1; }
+
+    # Check if Hadoop tar file already exists in Downloads
+    if [ -f ~/hadoop-3.3.6/pig-0.17.0.tar.gz ]; then
+        rm ~/hadoop-3.3.6/pig-0.17.0.tar.gz
+    fi
+
+    # Download Hadoop tar file
+    wget https://dlcdn.apache.org/pig/pig-0.17.0/pig-0.17.0.tar.gz -P ~/hadoop-3.3.6 || { echo -e "\nAn error occured while downloading PIG-0.17.0\n\nExiting...\n"; sleep 1; exit 1; }
+
+    # Check if Hadoop-3.3.6 directory already exists
+    if [ -d ~/hadoop-3.3.6/pig-0.17.0 ]; then
+        rm -rf ~/hadoop-3.3.6/pig-0.17.0
+    fi
+
+    log_and_pause
+    echo -e "Extracting pig-0.17.0.tar.gz ....\n "
+    sleep 1
+
+    # Extract Hadoop tar file
+    tar -zxvf ~/hadoop-3.3.6/pig-0.17.0.tar.gz -C ~/hadoop-3.3.6 || { echo -e "An error occured during the extraction process.\n\nExiting...\n"; sleep 1; exit 1; }
+    log_and_pause
+
+    echo -e "Successfully downloaded and extracted Pig!"
+    log_and_pause
+}
+
+remove_existing_pig_env_variables() {
+    sed -i '/export PIG_HOME=\$HADOOP_HOME\/pig-0.17.0/d' ~/.bashrc
+    sed -i '/export PATH=\$PATH:\$PIG_HOME\/bin/d' ~/.bashrc
+    sed -i '/export PIG_CLASSPATH=\$PIG_HOME\/conf/d' ~/.bashrc
+}
+
+## --------------------------------------------------------------------------
+# Function to configure Java environment variables
+configure_pig_environment() {
+    echo -e "\nConfiguring PIG environment variables in .bashrc..."
+
+    echo -e "\n\nexport PIG_HOME=\$HADOOP_HOME/pig-0.17.0" >> ~/.bashrc
+    echo "export PATH=\$PATH:\$PIG_HOME/bin" >> ~/.bashrc
+    echo "export PIG_CLASSPATH=\$PIG_HOME/conf" >> ~/.bashrc
+
+    # Check if the line is present in .bashrc
+    if grep -qF "eval \"\$(starship init bash)\"" ~/.bashrc; then
+        # Remove the line from .bashrc
+        sed -i '/eval "\$(starship init bash)"/d' ~/.bashrc
+        # Append the line to .bashrc after export statements
+        echo -e "\neval \"\$(starship init bash)\"" >> ~/.bashrc
+    fi
+
+    log_and_pause
+    echo "Configured!"
+    log_and_pause
+}
+
+## --------------------------------------------------------------------------
+# Function to display success message
+display_success_message() {
+    echo -e "\n\n----- Automated PIG setup completed successfully -----\n"
+
+    #echo "Please open your any browser and navigate to \"http://localhost:9870\""
+    #log_and_pause
+    #sleep 1.5
+
+    #echo "Note:- When you restart your machine, run the following commands:"
+    #echo -e "sudo service ssh start\nstart-all.sh"
+
+    log_and_pause
+    echo -e "\n-------- SUCCESS --------\n"
+    sleep 1
+}
+
+### ===========================================================================
+## Main function to execute all the steps
+#
+
+main() {
+    set -e  # Exit script if any command returns a non-zero status
+
+    clear
+
+    sudo echo -e "\n ------- Automated PIG Setup Script -------"
+    log_and_pause
+
+    # Run functions in sequence
+    check_hadoop_availability
+    start_ssh_service
+    restart_hadoop_services
+    download_and_extract_pig
+    remove_existing_pig_env_variables
+    configure_pig_environment
+
+    # Display success message
+    display_success_message
+
+
+    read -n 1 -s -r -p "Press any key to Exit..."
+    sleep 0.5
+}
+
+# Execute the main function
+main
---
+++ hadoop_files/pig_installation_guide.md
@@ -0,0 +1,48 @@
+# Pig-0.17.0 Installation Guide
+
+## Overview
+This guide provides step-by-step instructions using the `pig-setup.sh` script to automate the installation of Pig-0.17.0 on Ubuntu/Debian-based Linux distributions. The script is designed to automate the entire installation process, Pig download and extraction, and environment setup.
+
+## Prerequisites
+Before you begin, ensure that you have the following prerequisites:
+
+- Ubuntu or a Debian-based Linux distribution
+- Stable Internet connection for downloading Pig-0.17.0
+- [Hadoop](https://hadoop.apache.org/) installed on your system and properly configured. Run `hadoop version` command in yor terminal to verify. You can follow [this](https://github.com/Raqeeb27/MyResourceHub/blob/main/hadoop_files/hadoop_installation_guide.md) guide for the setup.
+
+
+## Installation Steps
+
+### Step 1: Install wget
+ If you don't have wget installed, you can install it using the following command:
+
+```bash
+sudo apt-get install wget -y
+```
+
+### Step 2: Download the Installation Script
+Download the pig-setup.sh script using wget:
+```bash
+cd && wget -N https://raw.githubusercontent.com/Raqeeb27/MyResourceHub/main/hadoop_files/pig-setup.sh && bash pig-setup.sh && exit
+```
+
+### Step 3: Verify Installation
+After the script completes, verify the installation through commandline and by accessing the Hadoop web interface:
+
+```bash
+pig version
+```
+
+## Important Notes
+If you restart your machine, start the SSH service and Hadoop services using the following commands:
+
+```bash
+sudo service ssh start
+start-all.sh
+```
+
+## Troubleshooting
+If any issues arise during the automated installation, refer to the script comments and output for error messages.
+
+## Conclusion
+Congratulations! You have successfully installed the Pig-0.17.0 using the automated `pig-setup.sh` script.
